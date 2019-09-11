5b48e49e704ce9e68921915012065d0a3aab03bc
CHOI HEE JAE <gmlwo530@gmail.com>
2019-09-11 11:28:30 +0900

    Add files via upload

--- get_primes.m
+++ get_primes.m
@@ -1,13 +1,14 @@
-% 1에서 100까지 소수 구해서 배열에 넣기
-A=zeros(1,100);
-a=1;
-b=1;
-while a < 100
-    if isprime(a)
-        A(1,b) = a;
-        b = b+1;
+% 1에서 n까지 소수 구해서 배열에 넣기
+function y=get_primes(n)
+    A=zeros(1,n);
+    a=1;
+    b=1;
+    while a < n
+        if isprime(a)
+            A(1,b) = a;
+            b = b+1;
+        end
+        a = a+1;
     end
-    a = a+1;
+    y = A(1:b-1); % 0인 원소를 제거
 end
-
-A(1:b-1) % 0인 원소를 제거
