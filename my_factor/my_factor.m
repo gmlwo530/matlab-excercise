46ada1bc43329d10f2945d331ad4adb90b607ea8
CHOI HEE JAE <gmlwo530@gmail.com>
2019-09-18 14:42:28 +0900

    Update my_factor.m

--- my_factor/my_factor.m
+++ my_factor/my_factor.m
@@ -13,20 +13,20 @@ function y = my_factor(n)
     b = 1;
     
     while a < n
-        if isprime(a) && ~mod(n,a)
-            if b > 1
+        if isprime(a) && ~mod(n,a) % 소수인지 체크하고, 해당 소수로 나뉘어 지는지 체크한다.
+            if b > 1 % 첫 번째로 해당 되는 소수를 제외한 숫자 앞에 곱셈 기호를 붙여준다.
                 y = y + 'x';
             end
             
             q = n;
-            e = 0;
+            e = 0; % 소수의 승으로 들어갈 변수이다.
             
-            while mod(q, a) == 0   
+            while mod(q, a) == 0  % n이 나뉘어지지 않을 때까지 loop를 돌린다
                 q = q / a;
                 e = e + 1;
             end
             
-            if e > 1
+            if e > 1 % 승이 1일 때 생략 하기 위한 분기이다
                 y = y + num2str(a) + '^' + num2str(e);
             else
                 y = y + num2str(a);
