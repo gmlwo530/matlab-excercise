807a86a9247f0ea95ec21176337edbfafc1aa270
CHOI HEE JAE <gmlwo530@gmail.com>
2019-10-22 21:43:18 +0900

    Create Lagrange_poly.m

---
+++ Lagrange_poly.m
@@ -0,0 +1,9 @@
+function p = Lagrange_poly(x,n,i)
+    p = zeros(1, n+1);
+    xi = x(i);
+    x(i) = []; % i번째 원소를 비움
+    
+%     denominator = ;
+% for 문은 한 번만 돌리기 nchoosek 써보기
+% denominator 이후로 일곱 줄 쓰기
+end
